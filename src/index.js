331cbfa3e35d9846dc2be8f1073ce06b5e3502ff
DAT <quoc@DATs-MacBook-Pro.local>
2024-04-18 16:39:14 +0700

    2- commit

--- src/index.js
+++ src/index.js
@@ -1,5 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+// import {composeWithDevTools} from "redux-devtools-extension"
+// import {crea} from "react-redux"
+import { store } from "./app/store";
+import { Provider } from "react-redux";
+// import reducer from "./components/book/bookSlice"
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+
+  document.getElementById("root")
+);
